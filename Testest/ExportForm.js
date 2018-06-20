e8006e733e5fab3b7a9f49dc49e9f589a2ec78a7
SneakyKenny <kennycreative@hotmail.fr>
2018-06-20 09:58:38 +0200

    On avance hein

--- Testest/ExportForm.js
+++ Testest/ExportForm.js
@@ -1,19 +1,18 @@
-if (!window.jQuery)
-{
-    var script = document.createElement('script');
-    script.type = "text/javascript";
-    script.src = "scripts/jquery-1.9.1.min.js";
-    document.getElementsByTagName("head")[0].appendChild(script);
-}
+$(function() {
+	$("#buttonZIP").on("click", function (event) {
+		event.preventDefault();
+		console.log("^_^");
 
-jQuery("body").on("click", ".buttonZIP", function (event) {
-    event.preventDefault();
-    $.ajax({
-        method: "POST",
-        url: "webservice/php/formExportController.php",
-        data: {nomenclature: jsNomenclature, appelAjax: 'oui', exportFormat: jQuery(this).attr("rel")}
-    }).done(function (data) {
-        
-    });
-    return false;
+		/* $.ajax({
+			type: 'POST',
+			url: 'formExportController', // url to the php file
+			data: ['create', 'zip'], // change that
+			success: function () {
+
+			}
+			error: function() {
+
+			}
+		}); */
+	});
 });
